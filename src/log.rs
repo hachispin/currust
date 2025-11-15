2082526002720de6c9aab8663e8338a75b0a0a03
hachispin <ganalonrainier255@gmail.com>
2025-11-15 18:32:35 +0000

    add more flags

---
+++ src/log.rs
@@ -0,0 +1,5 @@
+use simplelog;
+
+fn init_logging() {
+    todo!();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,84 @@
-use std::path::Path;
+use std::{fs, path::PathBuf};
 
 use clap::Parser;
-use log;
-use miette::Result;
+use log::{self, debug, info};
+use miette::{IntoDiagnostic, Result};
 use simplelog::Level;
 
+/// Represents received CLI arguments.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about=None)]
 struct Args {
+    /// The path to the Windows cursor
+    #[arg(value_name = "FILE", help_heading = "d")]
+    cursor_file: String,
+
     /// Disables logs entirely
     #[arg(short, long)]
     quiet: bool,
 
-    /// Shows all logs with severity ≥ LEVEL. Defaults to "warn"
-    #[arg(long, value_name = "LEVEL")]
-    log_level: Option<Level>,
+    /// Shows all logs with severity ≥ LEVEL
+    #[arg(long, value_name = "LEVEL", default_value_t = Level::Warn)]
+    log_level: Level,
 
     /// Writes logs to FILE instead of the terminal
     #[arg(long, value_name = "FILE")]
     log_file: Option<String>,
 }
 
-fn validate_args(args: &Args) -> Result<()> {
-    if let Some(v) = &args.log_file {
-        Path::canonicalize(v)?;
+/// [`Args`] as a pure data aggregate.
+///
+/// Some fields like [`Args::log_file`] are made to be more
+/// type correct here ([`PathBuf`] rather than [`String`])
+#[allow(unused)]
+#[derive(Debug)]
+struct ParsedArgs {
+    cursor_file: PathBuf,
+    quiet: bool,
+    log_level: Level,
+    log_file: Option<PathBuf>,
+}
+
+/// Validates the given `args`
+fn validate_args(args: Args) -> Result<ParsedArgs> {
+    let cursor_file = PathBuf::from(args.cursor_file)
+        .canonicalize()
+        .into_diagnostic()?;
+
+    let cursor_file_ext = cursor_file.extension().expect(&format!(
+        "failed to parse extension for cursor input {:?}",
+        cursor_file
+    ));
+
+    if cursor_file_ext != "cur" {
+        return Err(miette::miette!(
+            "expected extension `.cur`, got {}",
+            cursor_file_ext.display()
+        ));
     }
 
-    Ok(())
+    let log_file = args.log_file.map(|s| PathBuf::from(s));
+
+    if let Some(p) = &log_file {
+        info!("Creating log file");
+        fs::create_dir_all(p).into_diagnostic()?;
+        fs::write(p, "").into_diagnostic()?;
+    }
+
+    Ok(ParsedArgs {
+        cursor_file,
+        quiet: args.quiet,
+        log_level: args.log_level,
+        log_file,
+    })
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-
-    if let Some(v) = &args.log_file {
-        println!("totally writing to {v}");
-    }
+    debug!("raw_args={args:?}");
+    let args = validate_args(args)?;
 
     println!("{args:?}");
-    log::warn!("nothing");
 
     Ok(())
 }
