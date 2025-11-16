0ba01c9a2b81644301828cceaad57ee514cd63fa
hachispin <ganalonrainier255@gmail.com>
2025-11-16 21:06:24 +0000

    add basic blob unpacking of `ICONDIRENTRY` using `zerocopy`

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,47 @@
+use std::fs;
+
 use currust::{
     cli::{Args, validate_args},
+    errors::BlobError,
     logging::init_logging,
+    models::IconDirEntry,
 };
 
-use log::debug;
+use log::{debug, info};
 
 use clap::Parser;
-use miette::Result;
+use miette::{ErrReport, IntoDiagnostic, Result};
+use zerocopy::FromBytes;
+
+macro_rules! throw {
+    ($e:expr) => {
+        return Err(ErrReport::from($e))
+    };
+}
+
+// Reference: https://en.wikipedia.org/wiki/ICO_(file_format)#ICONDIR_structure
+const CUR_MAGIC: [u8; 4] = [0x0, 0x0, 0x2, 0x0];
 
 fn main() -> Result<()> {
     miette::set_panic_hook();
     let args = validate_args(Args::parse())?;
     init_logging(&args)?;
-
     debug!("args={args:?}");
+
+    let bytes: Vec<u8> = fs::read(&args.cursor_file).into_diagnostic()?;
+    let cursor_file_repr = &args.cursor_file.to_string_lossy();
+
+    if &bytes[0..4] != CUR_MAGIC {
+        throw!(BlobError::new(&bytes[0..4], cursor_file_repr));
+    }
+
+    let num_images = u16::from_le_bytes(bytes[4..6].try_into().into_diagnostic()?);
+    info!("idCount={num_images}");
+
+    let icon_dir_bytes: &[u8; 16] = bytes[6..22].try_into().into_diagnostic()?;
+    let icon_dir = IconDirEntry::read_from_bytes(icon_dir_bytes);
+
+    info!("Parsed ICONDIRENTRY: {icon_dir:?}");
 
     Ok(())
 }
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,33 @@
-//! NOT IMPLEMENTED
+//! Contains structs modeling byte layouts of Windows cursors.
+//!
+//! Note that `.cur` follow little-endian byte ordering.
 
-pub struct WinCursor {}
-pub struct X11Cursor {}
+use zerocopy::{
+    FromBytes, Immutable, KnownLayout,
+    little_endian::{U16 as u16_le, U32 as u32_le},
+};
 
+#[repr(C, packed)]
+#[derive(FromBytes, KnownLayout, Immutable, Debug)]
+/// Models the byte layout of `ICONDIRENTRY`, which
+/// stores info regarding an image (one image only)
+///
+/// Reference: https://en.wikipedia.org/wiki/ICO_(file_format)#ICONDIRENTRY_structure
+pub struct IconDirEntry {
+    /// width of stored image
+    width: u8,
+    /// height of stored image
+    height: u8,
+    /// irrelevant really
+    color_count: u8,
+    /// do not touch. must be 0--used for assertions
+    _reserved: u8,
+    // x-coordinate of cursor click pixel (i.e, hotspot)
+    hotspot_x: u16_le,
+    /// y-coordinate of cursor click pixel (i.e, hotspot)
+    hotspot_y: u16_le,
+    /// size of stored image in bytes
+    img_size: u32_le,
+    /// offset from start of `.cur` blob where png/bmp data starts
+    img_offset: u32_le,
+}
