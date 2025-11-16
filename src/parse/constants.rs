6f8b4efad1ed761938950b4966f40ec5f23de6ca
hachispin <ganalonrainier255@gmail.com>
2025-11-16 18:51:05 +0000

    formatting + wording

--- src/parse/constants.rs
+++ src/parse/constants.rs
@@ -1,6 +1,6 @@
 //! Stores constants, such as file headers and important offsets.
 //!
-//! Note: Windows formats (incl. cur) are generally little-endian.
+//! Note: Windows formats (incl. `.cur`) are generally little-endian.
 
 // https://en.wikipedia.org/wiki/ICO_(file_format)#NEWHEADER_structure
 pub const CUR_MAGIC: [u8; 4] = [0x00, 0x00, 0x02, 0x00];
@@ -10,8 +10,8 @@ pub const CUR_MAGIC: [u8; 4] = [0x00, 0x00, 0x02, 0x00];
 
 nofmt::pls! {
 
-pub const NUM_IMAGES_OFFSET: usize = 4;
-pub const ICONDIRENTRY_ARRAY_OFFSET: usize = 6;
+pub const NUM_IMAGES_OFFSET         : usize = 4;
+pub const ICONDIRENTRY_ARRAY_OFFSET : usize = 6;
 
 }
 
@@ -23,8 +23,8 @@ pub const ICONDIRENTRY_ARRAY_OFFSET: usize = 6;
 //
 // i.e, blob[OFFSET_IMG_DATA_OFFSET] is where image data starts.
 //
-// The same for `SIZE_IMG_BYTE_SIZE`, it's the size of the data
-// type (e.g, u32) that stores the number of bytes in the image.
+// The same applies for `SIZE_IMG_BYTE_SIZE`, it's the size of the
+// data type (e.g, u32) that stores the number of bytes in the image.
 
 nofmt::pls! {
 
