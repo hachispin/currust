69c2679ad0a1007b6909e6f070955df63893b197
hachispin <ganalonrainier255@gmail.com>
2025-11-16 23:28:32 +0000

    fixed lifetimes so `zerocopy` is usable
    to be more specific, the given binary blob is now a field in `WinCursor`.
    `WinCursor`s lifetime parameters are also passed to `IconDir`.
    
    this ensures all references to the blob stay valid for
    runtime, which is the guarantee that `zerocopy` wants.

--- src/main.rs
+++ src/main.rs
@@ -28,20 +28,5 @@ fn main() -> Result<()> {
     init_logging(&args)?;
     debug!("args={args:?}");
 
-    let bytes: Vec<u8> = fs::read(&args.cursor_file).into_diagnostic()?;
-    let cursor_file_repr = &args.cursor_file.to_string_lossy();
-
-    if &bytes[0..4] != CUR_MAGIC {
-        throw!(BlobError::new(&bytes[0..4], cursor_file_repr));
-    }
-
-    let num_images = u16::from_le_bytes(bytes[4..6].try_into().into_diagnostic()?);
-    info!("idCount={num_images}");
-
-    let icon_dir_bytes: &[u8; 16] = bytes[6..22].try_into().into_diagnostic()?;
-    let icon_dir = IconDirEntry::read_from_bytes(icon_dir_bytes);
-
-    info!("Parsed ICONDIRENTRY: {icon_dir:?}");
-
-    Ok(())
+    todo!();
 }
--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,10 @@
 //! Contains structs modeling byte layouts of Windows cursors.
 //!
-//! Note that `.cur` follow little-endian byte ordering.
+//! Note that the `.cur` format follows little-endian byte ordering.
 
+use std::{fs, path::Path};
+
+use miette::{IntoDiagnostic, Result};
 use zerocopy::{
     FromBytes, Immutable, KnownLayout,
     little_endian::{U16 as u16_le, U32 as u32_le},
@@ -9,25 +12,82 @@ use zerocopy::{
 
 #[repr(C, packed)]
 #[derive(FromBytes, KnownLayout, Immutable, Debug)]
+/// Partially models the byte layout of `ICONDIR`.
+///
+/// This does not contain `idEntries` due to it
+/// being variable-sized (with length `img_count`).
+///
+/// Reference: https://en.wikipedia.org/wiki/ICO_(file_format)#ICONDIR_structure
+struct IconDirHeader {
+    /// must be 0
+    _reserved: u16_le,
+    /// 1 = ICO, 2 = CUR. other values are invalid
+    type_: u16_le,
+    img_count: u16_le,
+}
+
+/// The full model of `ICONDIR`, including `idEntries`
+///
+/// Lifetime `'b` indicates that the blob these fields
+/// point to must be valid for as long as [`IconDir`] is.
+pub struct IconDir<'a> {
+    _reserved: &'a u16_le,
+    type_: &'a u16_le,
+    img_count: &'a u16_le,
+    img_entries: &'a [IconDirEntry<'a>],
+}
+
+// impl IconDir {
+//     /// Parses `bytes` and does any dirty work.
+//     ///
+//     /// Use this over [`IconDirHeader::read_from_bytes`].
+//     pub fn new(bytes: &[u8]) -> Result<Self> {
+//         let (header, entries) = IconDirHeader::read_from_prefix(bytes).into_diagnostic()?;
+
+//         let entry_size = size_of::<IconDirEntry>();
+//         assert_eq!(entry_size, 16);
+//         let end = usize::from(header.img_count) * entry_size;
+//         let entries: Vec<IconDirEntry> = Vec::with_capacity(usize::from(header.img_count));
+
+//         panic!();
+//     }
+// }
+
+#[repr(C, packed)]
+#[derive(FromBytes, KnownLayout, Immutable, Debug)]
 /// Models the byte layout of `ICONDIRENTRY`, which
 /// stores info regarding an image (one image only)
 ///
 /// Reference: https://en.wikipedia.org/wiki/ICO_(file_format)#ICONDIRENTRY_structure
-pub struct IconDirEntry {
+pub struct IconDirEntry<'b> {
     /// width of stored image
-    width: u8,
+    width: &'b u8,
     /// height of stored image
-    height: u8,
+    height: &'b u8,
     /// irrelevant really
-    color_count: u8,
-    /// do not touch. must be 0--used for assertions
-    _reserved: u8,
+    color_count: &'b u8,
+    /// must be 0
+    _reserved: &'b u8,
     // x-coordinate of cursor click pixel (i.e, hotspot)
-    hotspot_x: u16_le,
+    hotspot_x: &'b u16_le,
     /// y-coordinate of cursor click pixel (i.e, hotspot)
-    hotspot_y: u16_le,
+    hotspot_y: &'b u16_le,
     /// size of stored image in bytes
-    img_size: u32_le,
+    img_size: &'b u32_le,
     /// offset from start of `.cur` blob where png/bmp data starts
-    img_offset: u32_le,
+    img_offset: &'b u32_le,
+}
+
+struct WinCursor<'blob> {
+    blob: Vec<u8>,
+    /// lives as long as WinCursor so blob stays usable
+    icon_dir: IconDir<'blob>,
+}
+
+impl WinCursor<'_> {
+    pub fn new(fp: &Path) -> Result<Self> {
+        let bytes = fs::read(fp).into_diagnostic()?;
+
+        todo!();
+    }
 }
