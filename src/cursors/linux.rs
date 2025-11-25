31ce61a035ceefe2e7a3db3b0c6f982fbd483ae9
hachispin <ganalonrainier255@gmail.com>
2025-11-25 21:04:04 +0000

    add swap function for RGBA => ARGB

--- src/cursors/linux.rs
+++ src/cursors/linux.rs
@@ -1,1 +1,17 @@
 //! WIP
+
+//  https://man.archlinux.org/man/Xcursor.3
+
+/// Converts the bytes in `rgba` to ARGB format in-place.
+pub fn to_argb(rgba: &mut Vec<u8>) {
+    assert!(
+        rgba.len() % 4 == 0,
+        "invalid RGBA, each pixel should have 4 channels"
+    );
+
+    for pixel in rgba.chunks_exact_mut(4) {
+        pixel.swap(0, 3); // AGBR
+        pixel.swap(1, 2); // ABGR
+        pixel.swap(1, 3); // ARGB
+    }
+}
