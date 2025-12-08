6159ebc1c4a5003688135c0103881ff9629ad713
hachispin <ganalonrainier255@gmail.com>
2025-12-08 20:39:33 +0000

    tweak for xcur tests

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-use std::{fs::File, io::BufWriter};
-
 use currust::{
     cli::{Args, validate_args},
-    cursors::{common::CursorImage, windows::WinCursor},
+    cursors::{common::CursorImage, linux::Xcursor, windows::WinCursor},
     logging::init_logging,
 };
 
 use clap::Parser;
 use log::debug;
-use miette::{IntoDiagnostic, Result};
-use png::{BitDepth, ColorType, Encoder};
+use miette::Result;
 
 fn main() -> Result<()> {
     miette::set_panic_hook();
@@ -17,23 +14,14 @@ fn main() -> Result<()> {
     init_logging(&args)?;
     debug!("args={args:?}");
 
-    for (i, cursor_path) in args.cursor_paths.into_iter().enumerate() {
+    for cursor_path in args.cursor_paths {
         let cur = WinCursor::new(&cursor_path)?;
         let cursor_images = CursorImage::from_win_cur(&cur)?;
 
-        for (j, cursor_image) in cursor_images.into_iter().enumerate() {
-            let path = args.out.join(format!("{i}-{j}.png"));
-            let file = File::create(path).into_diagnostic()?;
-            let w = &mut BufWriter::new(file);
-
-            let mut encoder = Encoder::new(w, cursor_image.width, cursor_image.height);
-            encoder.set_depth(BitDepth::Eight);
-            encoder.set_color(ColorType::Rgba);
-
-            let mut writer = encoder.write_header().into_diagnostic()?;
-            writer
-                .write_image_data(&cursor_image.rgba)
-                .into_diagnostic()?;
+        for cursor_image in cursor_images {
+            let path = args.out.join("xcur_testing/left_ptr");
+            let xcur = Xcursor::from_cursor_image(&[cursor_image]);
+            xcur.save(&path)?;
         }
     }
 
