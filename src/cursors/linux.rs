baab284e24bfa125d8d9ce68f68b9c449715c807
hachispin <ganalonrainier255@gmail.com>
2025-12-08 20:38:49 +0000

    doing some experimenting (non-functional)

--- src/cursors/linux.rs
+++ src/cursors/linux.rs
@@ -15,9 +15,10 @@
 use std::{fs::File, io::Cursor, path::Path};
 
 use binrw::BinWrite;
-use miette::{Result, IntoDiagnostic};
+use log::warn;
+use miette::{Context, IntoDiagnostic, Result};
 
-use crate::cursors::common::CursorImage;
+use super::common::CursorImage;
 
 /// Converts the bytes in `rgba` to ARGB format in-place.
 ///
@@ -38,12 +39,12 @@ pub fn to_argb(rgba: &mut [u8]) {
 }
 
 #[derive(BinWrite)]
-#[bw(little, magic = b"Xcur")]
+#[bw(little, magic = b"Xcur", assert(*num_toc as usize == tocs.len()))]
 struct XcursorHeader {
     header_size: u32,
     version: u32,
     num_toc: u32,
-    tocs: TableOfContents,
+    tocs: Vec<TableOfContents>,
 }
 
 /// The fields here are specific to image chunks,
@@ -52,25 +53,37 @@ struct XcursorHeader {
 /// Only the [`Self::position`] field is useful here,
 /// everything else is redundant/duplicated in the chunk.
 #[derive(BinWrite)]
-#[bw(little, assert(*_type == 0xfffd_0002))]
+#[bw(little, assert(*type_id == TableOfContents::IMG_TYPE_ID))]
 struct TableOfContents {
-    _type: u32,
+    type_id: u32,
     /// Also known as the subtype.
     _nominal_size: u32,
     position: u32,
 }
 
-/// Prefer using the fields here over the
-/// ones stored in [`TableOfContents`].
+// For constants
+impl TableOfContents {
+    /// Value used to indicate a chunk is an image chunk.
+    const IMG_TYPE_ID: u32 = 0xfffd_0002;
+
+    /// Header size for image chunks.
+    const IMG_HEADER_SIZE: u32 = 36;
+
+    const VERSION: u32 = 1;
+}
+
+/// Prefer using the fields here over the ones stored in [`TableOfContents`].
+///
+/// Use [`Self::new`] to handle invariant fields.
 #[derive(BinWrite)]
 #[bw(
     little,
-    assert(*header_size == 36),
-    assert(*_type == 0xfffd_0002)
+    assert(*header_size == TableOfContents::IMG_HEADER_SIZE),
+    assert(*type_id == TableOfContents::IMG_TYPE_ID)
 )]
 struct ImageChunkHeader {
     header_size: u32,
-    _type: u32,
+    type_id: u32,
     /// Also known as the subtype.
     nominal_size: u32,
     version: u32,
@@ -81,6 +94,22 @@ struct ImageChunkHeader {
     delay: u32,
 }
 
+impl From<&CursorImage> for ImageChunkHeader {
+    fn from(cursor: &CursorImage) -> Self {
+        Self {
+            header_size: TableOfContents::IMG_HEADER_SIZE,
+            type_id: TableOfContents::IMG_TYPE_ID,
+            nominal_size: cursor.height.max(cursor.width),
+            version: TableOfContents::VERSION,
+            width: cursor.width,
+            height: cursor.height,
+            hotspot_x: cursor.hotspot_x,
+            hotspot_y: cursor.hotspot_y,
+            delay: 0, // Only matters in animated cursors
+        }
+    }
+}
+
 #[derive(BinWrite)]
 #[bw(little, assert(argb.len() == (header.width * header.height * 4) as usize))]
 struct ImageChunk {
@@ -88,43 +117,74 @@ struct ImageChunk {
     argb: Vec<u8>,
 }
 
-impl ImageChunk {
-    /// Consumes the `cursor` to construct an [`ImageChunk`].
-    fn from_cursor_image(cursor: CursorImage) -> Self {
-        let header = ImageChunkHeader {
-            header_size: 36,
-            _type: 0xfffd_0002,
-            nominal_size: cursor.height * cursor.width,
-            version: 1,
-            width: cursor.width,
-            height: cursor.height,
-            hotspot_x: cursor.hotspot_x,
-            hotspot_y: cursor.hotspot_y,
-            delay: 0,
-        };
-
-        let mut argb = cursor.rgba;
+impl From<&CursorImage> for ImageChunk {
+    fn from(cursor: &CursorImage) -> Self {
+        let header = ImageChunkHeader::from(cursor);
+        let mut argb = cursor.rgba.clone();
         to_argb(&mut argb);
 
-        Self {
-            header,
-            argb,
-        }
+        Self { header, argb }
     }
 }
 
+/// Represents the structure of an Xcursor file.
 #[derive(BinWrite)]
-struct Xcursor {
+pub struct Xcursor {
     header: XcursorHeader,
-    blob: Vec<u8>,
+    chunks: Vec<ImageChunk>,
 }
 
 impl Xcursor {
-    fn from_cursor_image(cursor: &[CursorImage]) {
-        todo!();
+    /// Conversion function between generic cursors and Xcursor.
+    ///
+    /// ## Panics
+    ///
+    /// If `cursor` is empty.
+    #[must_use]
+    pub fn from_cursor_image(cursor: &[CursorImage]) -> Self {
+        assert!(!cursor.is_empty(), "`cursor` cannot be empty");
+
+        if cursor.len() > 1 {
+            warn!("Unstable feature: creating `Xcursor` from multiple cursor images");
+        }
+
+        let cursor = cursor.first().unwrap();
+        let chunk = ImageChunk::from(cursor);
+
+        let toc_pos = 40u32;
+
+        #[allow(clippy::cast_possible_truncation)]
+        let toc = TableOfContents {
+            type_id: chunk.header.type_id,
+            _nominal_size: chunk.header.nominal_size,
+            position: toc_pos,
+        };
+
+        let header = XcursorHeader {
+            header_size: 24,
+            version: 1,
+            num_toc: 1,
+            tocs: vec![toc],
+        };
+
+        Self {
+            header,
+            chunks: vec![chunk],
+        }
     }
 
-    fn save() {
-        todo!();
+    /// Saves the current Xcursor the `path`.
+    ///
+    /// ## Errors
+    ///
+    /// If `path` can't be created.
+    pub fn save(&self, path: &Path) -> Result<()> {
+        let mut file = File::create(path)
+            .into_diagnostic()
+            .with_context(|| format!("Failed to create file {}", path.display()))?;
+
+        self.write_le(&mut file);
+
+        Ok(())
     }
 }
