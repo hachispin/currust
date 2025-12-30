845b39f2c440869529836af9fbed093168bbe84f
hachispin <ganalonrainier255@gmail.com>
2025-12-30 01:28:18 +0000

    add INFO chunk "parsing" (skipping)

--- src/cursors/ani.rs
+++ src/cursors/ani.rs
@@ -9,10 +9,10 @@
 
 use super::generic_cursor::GenericCursor;
 
-use std::{fs, path::Path};
+use std::{fs, io::SeekFrom, path::Path};
 
 use anyhow::{Context, Result};
-use binrw::BinRead;
+use binrw::{BinRead, binread};
 use ico::IconDir;
 
 /// All chunks follow this structure:
@@ -37,24 +37,13 @@ struct RiffChunk {
     // padding byte is skipped with `pad_after`
 }
 
-#[derive(Debug, BinRead)]
-#[br(
-    magic = b"LISTfram", // hack
-    assert(frames.iter().all(|f| f.id == *b"icon")),
-)]
-struct AniFrames {
-    num_frames: u32,
-    #[br(count=num_frames)]
-    frames: Vec<RiffChunk>,
-}
-
 /// Contains possible flag combinations for [`AniHeader`].
 /// These are used to describe the state of the "seq " chunk.
 ///
 /// Invalid flags:
 ///
-/// - `0x0`: no flags set
-/// - `0x2`: frames are not ICO
+/// - `0`: no flags set
+/// - `2`: frames are not ICO
 ///
 /// All frames must be in ICO format.
 #[derive(Debug, PartialEq, BinRead)]
@@ -64,29 +53,79 @@ enum AniFlags {
     /// which defines the order frames should be played.
     ///
     /// This is mainly for optimizing repeated frames(?).
-    SequencedIcon = 0x1,
+    SequencedIcon = 1,
     /// Contains ICO frames that play in the
     /// order they're defined (no "seq " chunk).
-    UnsequencedIcon = 0x3,
+    UnsequencedIcon = 3,
 }
 
 /// Models an ANI file's header.
 ///
-/// ## Refernces
+/// ## References
 ///
 /// - [Wikipedia: ANI structure](https://en.wikipedia.org/wiki/ANI_(file_format)#File_structure)
-#[derive(Debug, BinRead)]
+#[binread]
+#[derive(Debug)]
 #[br(magic = b"anih")]
+#[br( // reference:  https://www.gdgsoft.com/anituner/help/aniformat.htm
+    assert(_cx == 0 && _cy == 0 && _c_bit_count == 0 && _c_planes == 0,
+        "_cx, _cy, _c_bit_count and _c_planes are reserved and must be 0"
+    ),
+)]
 struct AniHeader {
     header_size: u32,
     num_frames: u32,
-    // skip four DWORDs for unused fields: cx, cy, cBitCount, cPlanes
-    #[br(pad_after = size_of::<u32>() * 4)]
     num_steps: u32,
+
+    #[br(temp)]
+    _cx: u32,
+    #[br(temp)]
+    _cy: u32,
+    #[br(temp)]
+    _c_bit_count: u32,
+    #[br(temp)]
+    _c_planes: u32,
+
     jiffle_rate: u32,
     flags: AniFlags,
 }
 
-#[derive(Debug, BinRead)]
-#[br(magic = b"RIFF")]
-struct AniFile {/* TODO: Finish this */}
+/// This has no fields, doesn't parse metadata,
+/// and is only used to skip until [`AniHeader`].
+///
+/// _no one even writes this chunk anyway..._
+#[binread]
+#[derive(Debug)]
+#[br(magic = b"INFO")]
+struct SkipAniMetadata {
+    // this chunk (that we're skipping) is just two strings max, so
+    #[br(assert(_list_size < 1024, "INFO chunk unreasonably large"), temp)]
+    _list_size: u32,
+
+    // just skip all the metadata to jump to `AniHeader`
+    // RIFF adds padding to make chunk sizes even
+    #[br(calc = _list_size % 2, temp)]
+    _padding: u32,
+    #[br(pad_after = (_list_size + _padding), temp)]
+    _skip: (),
+
+    // make sure we skipped far enough, to `AniHeader`
+    #[br(assert(_anih == *b"anih"), temp)]
+    _anih: [u8; 4],
+
+    // step back so `AniHeader` can assert its magic
+    #[br(pad_after = -4, temp)]
+    _back: (),
+}
+
+#[binread]
+#[derive(Debug)]
+#[br(magic = b"RIFFACON")]
+struct AniFile {
+    first_id: [u8; 4],
+    #[br(if(first_id == *b"LIST"), temp)]
+    _metadata: Option<SkipAniMetadata>,
+    header: AniHeader,
+    second_id: [u8; 4],
+    /* TODO: Finish this */
+}
