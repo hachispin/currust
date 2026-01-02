0cfc48c72a85608abf5baec721cfdfc52eaffce9
hachispin <ganalonrainier255@gmail.com>
2026-01-02 16:01:13 +0000

    update docs to explicitly allow ani

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 //! Module for [`clap`] code.
 //!
 //! This contains the [`Args`] struct, which has the [`Parser`]
-//! trait, and the [`ParsedArgs`] struct, which is just plain old data.s
+//! trait, and the [`ParsedArgs`] struct, which is just plain old data.
 
 use std::{
     fs,
@@ -15,7 +15,7 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct Args {
-    /// The path to a CUR file, or a directory containing CUR files.
+    /// The path to a CUR/ANI file, or a directory containing CUR/ANI files.
     path: String,
 
     /// Where to place parsed Xcursors.
@@ -29,8 +29,8 @@ pub struct Args {
 /// Parsed CLI arguments.
 #[derive(Debug)]
 pub struct ParsedArgs {
-    /// All files in the specified directory that are CUR files.
-    pub cur_paths: Vec<PathBuf>,
+    /// All files in the specified directory that are CUR/ANI files.
+    pub cursor_paths: Vec<PathBuf>,
     /// Where to put parsed Xcursor files.
     pub out: PathBuf,
 }
@@ -43,18 +43,18 @@ impl ParsedArgs {
     ///
     /// ## Errors
     ///
-    /// If the input path is to a directory that doesn't contain
-    /// CUR files, or to a file that lacks the `.cur` extension.
+    /// If the input path is to a directory that doesn't contain CUR or
+    /// ANI files, or to a file that lacks the `.cur`/`.ani` extension.
     pub fn from_args(args: &Args) -> Result<Self> {
-        let cur_paths = Self::validate_cur_path(&args.path)?;
+        let cur_paths = Self::validate_cursor_path(&args.path)?;
         let out = PathBuf::from(&args.out);
         fs::create_dir_all(&out).with_context(|| format!("failed to create out={}", args.out))?;
 
-        Ok(Self { cur_paths, out })
+        Ok(Self { cursor_paths: cur_paths, out })
     }
 
     /// Helper function for validating [`Args::path`].
-    fn validate_cur_path(path: &str) -> Result<Vec<PathBuf>> {
+    fn validate_cursor_path(path: &str) -> Result<Vec<PathBuf>> {
         // for triage purposes
         let path_str = path.to_string();
 
@@ -63,7 +63,7 @@ impl ParsedArgs {
             .with_context(|| format!("failed to canonicalize path {path_str}"))?;
 
         if path.is_dir() {
-            let cur_paths = Self::extract_curs(&path)?;
+            let cur_paths = Self::extract_cursors(&path)?;
 
             if !cur_paths.is_empty() {
                 return Ok(cur_paths);
@@ -85,23 +85,23 @@ impl ParsedArgs {
         bail!("couldn't coerce {path_str} as a dir or file")
     }
 
-    /// Returns all the files in `dir` that point
-    /// to CUR files. (files with CUR extension)
-    fn extract_curs(cur_dir: &Path) -> Result<Vec<PathBuf>> {
+    /// Returns all the files in `dir` that point to
+    /// "cursor" files. (files with `.cur` or `.ani` extension)
+    fn extract_cursors(cursor_dir: &Path) -> Result<Vec<PathBuf>> {
         assert!(
-            cur_dir.is_dir(),
+            cursor_dir.is_dir(),
             "passed `cur_dir` to `extract_curs()` must be a dir"
         );
 
-        let mut cur_paths = Vec::new();
-        let cur_dir_display = cur_dir.display();
-        let entries = cur_dir
+        let mut cursor_paths = Vec::new();
+        let path_display = cursor_dir.display();
+        let entries = cursor_dir
             .read_dir()
-            .with_context(|| format!("failed to read entries of cur_dir={cur_dir_display}"))?;
+            .with_context(|| format!("failed to read entries of cur_dir={path_display}"))?;
 
         for entry in entries {
             let entry = entry.with_context(|| {
-                format!("`entries` iterator over cur_dir={cur_dir_display} yielded bad item")
+                format!("`entries` iterator over cur_dir={path_display} yielded bad item")
             })?;
 
             let entry_path = entry.path();
@@ -109,10 +109,10 @@ impl ParsedArgs {
             if let Some(ext) = entry_path.extension()
                 && (ext == "cur" || ext == "ani")
             {
-                cur_paths.push(entry_path);
+                cursor_paths.push(entry_path);
             }
         }
 
-        Ok(cur_paths)
+        Ok(cursor_paths)
     }
 }
