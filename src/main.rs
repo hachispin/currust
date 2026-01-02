c3c9b7c7607adf692e6d1d76b139bcd0b438a452
hachispin <ganalonrainier255@gmail.com>
2026-01-02 12:08:51 +0000

    use `rayon` for parallelism (from testing, around 3-4x faster for 15 ANI)

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,29 @@ use currust::{
 
 use anyhow::Result;
 use clap::Parser;
+use rayon::prelude::*;
 
 fn main() -> Result<()> {
     let raw_args = Args::parse();
     let args = ParsedArgs::from_args(&raw_args)?;
-    let test = &args.cur_paths[0];
 
-    let mut cursor = GenericCursor::from_ani_path(test)?;
-    cursor.add_scale(2, Upscale)?;
-    cursor.add_scale(3, Upscale)?;
-    dbg!(&cursor.scaled_images().len());
-    cursor.save_as_xcursor(args.out.join("left_ptr"))?;
+    // 2. use par_iter for parallel processing
+    args.cur_paths
+        .par_iter()
+        .try_for_each(|path| -> Result<()> {
+            let filename = path.file_stem().unwrap();
+            let out = args.out.join(filename);
+
+            // parse & manipulate the cursor as before
+            let mut cursor = GenericCursor::from_ani_path(path)?;
+            cursor.add_scale(2, Downscale)?;
+            cursor.add_scale(2, Upscale)?;
+            cursor.add_scale(3, Upscale)?;
+            cursor.add_scale(4, Upscale)?;
+            cursor.save_as_xcursor(out)?;
+
+            Ok(())
+        })?;
 
     Ok(())
 }
