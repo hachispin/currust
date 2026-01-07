8d7b0546be2f1021f22559fb391718574656cfdf
hachispin <ganalonrainier255@gmail.com>
2026-01-07 19:49:16 +0000

    add more error handling

--- testing/load_xcur.c
+++ testing/load_xcur.c
@@ -3,13 +3,14 @@
 #include <X11/Xcursor/Xcursor.h>
 #include <X11/Xlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char** argv) {
     char* cursor_name = "left_ptr";
-    int size = 32;
+    int32_t size = 32;
 
     const char* usage_yap = "usage: ./load_xcur [cursor_name=left_ptr] [size=32]\n";
 
@@ -26,8 +27,8 @@ int main(int argc, char** argv) {
     if (argc == 3) {
         size = atoi(argv[2]);
 
-        if (size == 0) {
-            fprintf(stderr, "size can't be 0 (or invalid size arg)\n");
+        if (size <= 0) {
+            fprintf(stderr, "size must be greater than zero (or invalid size arg)\n");
             fprintf(stderr, "%s", usage_yap);
             return 1;
         }
@@ -36,42 +37,74 @@ int main(int argc, char** argv) {
     Display* display = XOpenDisplay(NULL);
 
     if (!display) {
-        fprintf(stderr, "`XOpenDisplay()` failed\n");
+        fprintf(stderr, "XOpenDisplay() failed\n");
         return 1;
     }
 
-    Window window = XCreateSimpleWindow(display, DefaultRootWindow(display), 100, 100, 300, 200, 1, 0, 0xffffff);
-    XStoreName(display, window, "xcursor test");
+    const int32_t WINDOW_X = 100;
+    const int32_t WINDOW_Y = 100;
+    const uint32_t WIDTH = 600;
+    const uint32_t HEIGHT = 400;
+    const uint32_t BORDER_WIDTH = 1;
+    const uint64_t BORDER_COLOR = 0;            // black
+    const uint64_t BACKGROUND_COLOR = 0xFFFFFF; // white
+
+    // create window to display cursor in
+    Window window = XCreateSimpleWindow(
+        display, DefaultRootWindow(display),
+        WINDOW_X, WINDOW_Y,
+        WIDTH, HEIGHT,
+        BORDER_WIDTH, BORDER_COLOR,
+        BACKGROUND_COLOR);
+
+    if (!window) {
+        fprintf(stderr, "XCreateSimpleWindow() failed\n");
+        return 1;
+    }
+
+    // set title and do some weird stuff
+    XStoreName(display, window, "Xcursor test (currust)");
     XSelectInput(display, window, ExposureMask | StructureNotifyMask);
     XMapWindow(display, window);
+
     XcursorImages* image = XcursorFilenameLoadImages(cursor_name, size);
 
     if (!image) {
-        fprintf(stderr, "`XcursorFilenameLoadImages()` failed\n");
+        fprintf(stderr, "XcursorFilenameLoadImages() failed\n");
         return 1;
     }
 
-    Cursor* frames = malloc(sizeof(Cursor) * (size_t) image->nimage);
+    // i.e, length of frames array
+    const size_t num_frames = (size_t) image->nimage;
+
+    if (num_frames == 0) {
+        fprintf(stderr, "no frames (nimage == 0)\n");
+        return -1;
+    }
+
+    Cursor* frames = malloc(sizeof(Cursor) * num_frames);
 
     if (!frames) {
-        fprintf(stderr, "`malloc()` failed\n");
+        fprintf(stderr, "malloc() failed\n");
         return 1;
     }
 
-    for (int i = 0; i < image->nimage; ++i) {
+    for (size_t i = 0; i < num_frames; ++i) {
         frames[i] = XcursorImageLoadCursor(display, image->images[i]);
     }
 
-    // might leak but probably not
-    int frame = 0;
+    // current index to frames array
+    size_t frame = 0;
     while (true) {
         XDefineCursor(display, window, frames[frame]);
         XFlush(display);
 
-        unsigned int delay = image->images[frame]->delay;
+        uint32_t delay = image->images[frame++]->delay;
+        frame = frame % num_frames;
 
-        // convert to microseconds
+        // sleep before next frame, convert to microseconds
         usleep(delay * 1000);
-        frame = (frame + 1) % image->nimage;
     }
+
+    // this might leak but oh well. no frees for you
 }
