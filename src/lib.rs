76062c3b5079e2d968a888806723f2ccd55d4413
hachispin <ganalonrainier255@gmail.com>
2026-01-12 19:07:12 +0000

    add some lints for mistakes i often make

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 #![doc = include_str!("../README.md")]
-#![warn(clippy::pedantic)]
-#![warn(missing_docs)]
+#![warn(
+    clippy::pedantic,
+    // nursery lints:
+    clippy::use_self,
+    clippy::or_fun_call,
+    clippy::redundant_clone,
+    clippy::equatable_if_let,
+    clippy::needless_collect,
+    // restriction lints:
+    clippy::redundant_type_annotations,
+    clippy::semicolon_inside_block,
+    // annoying one:
+    missing_docs
+)]
 
 pub mod cli;
 pub mod cursors;
