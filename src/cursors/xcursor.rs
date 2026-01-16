7a5d600cb8ab0b9e23bfc927087567224df3c6a1
hachispin <ganalonrainier255@gmail.com>
2026-01-16 00:33:08 +0000

    add more asserts and docs

--- src/cursors/xcursor.rs
+++ src/cursors/xcursor.rs
@@ -1,17 +1,21 @@
-//! Contains writing logic for [Xcursor](https://manpages.ubuntu.com/manpages/plucky/man3/Xcursor.3.html).
+//! Module used to write Xcursor from [`GenericCursor`].
 //!
-//! Also useful: [libXcursor source](https://gitlab.freedesktop.org/xorg/lib/libxcursor)
-//!
-//! In summary, the format is stored as such:
+//! The Xcursor format is stored as such:
 //!
 //! 1) Magic bytes "Xcur", indicating it's, well, Xcursor.
 //! 2) The [`XcursorHeader`], which contains the required [`TableOfContents`].
-//!    Each of these entries point to an [`ImageChunk`].
-//! 3) The image chunks, which store big-endian ARGB pixel data,
+//!    Each of these entries point to either an image or comment chunk.
+//! 3) The [`ImageChunk`] s, which store **big-endian ARGB** pixel data,
 //!    along with other required metadata such as the hotspot.
 //!
 //! NOTE: Comment chunks aren't handled to make code
 //!       simpler, we're only _writing_, after all.
+//!
+//! ## References
+//!
+//! - [Xcursor](https://manpages.ubuntu.com/manpages/plucky/man3/Xcursor.3.html)
+//! - [libXcursor source](https://gitlab.freedesktop.org/xorg/lib/libxcursor)
+//! - [xcursorgen](https://gitlab.freedesktop.com/xorg/app/xcursorgen)
 // god i don't wanna write a parser again
 
 use super::{cursor_image::CursorImage, generic_cursor::GenericCursor};
@@ -19,22 +23,24 @@ use super::{cursor_image::CursorImage, generic_cursor::GenericCursor};
 use anyhow::Result;
 use binrw::binwrite;
 
+/* Sizes are needed since binrw "deletes" calculated fields. */
+
 /// Current Xcursor version. May need updating in the future.
 const XCURSOR_HEADER_VERSION: u32 = 1 << 16;
 
-/// Size of [`XcursorHeader`], including its magic and excluding the table of contents.
+/// Size of [`XcursorHeader`], including elided fields and its magic, excluding `toc`.
 const XCURSOR_HEADER_SIZE: u32 = 16;
 
-/// Size of [`TableOfcontents`].
+/// Size of [`TableOfContents`], including elided fields.
 const TOC_SIZE: u32 = 12;
 
-/// Size of [`ImageChunk`].
+/// Size of [`ImageChunk`], excluding `argb`.
 const IMAGE_HEADER_SIZE: u32 = 36;
 
-/// Magic value used to indicate an image chunk.
+/// Magic value used to indicate a chunk is an [`ImageChunk`].
 const IMAGE_TYPE: u32 = 0xfffd_0002;
 
-/// Do I have to keep writing docs?
+/// Current version stored in [`ImageChunk`].
 const IMAGE_VERSION: u32 = 1;
 
 /// Represents the file header for Xcursor files.
@@ -48,13 +54,14 @@ struct XcursorHeader {
     version: u32,
     #[bw(try_calc = toc.len().try_into())]
     num_toc: u32,
+
     /// TOC entries which lead to [`ImageChunk`] s.
     toc: Vec<TableOfContents>,
 }
 
-/// A table of contents stores metadata regarding a chunk.
-// technically this should be named "TableOfContentsEntry"
-// but that's too annoying to type for me... ¯\_(ᵕ—ᴗ—)_/¯
+/// A table of contents that stores metadata regarding a chunk.
+///
+/// This should always lead to an [`ImageChunk`].
 #[binwrite]
 #[derive(Debug, Clone)]
 #[bw(little)]
@@ -86,31 +93,35 @@ struct ImageChunk {
     header_size: u32,
     #[bw(calc = IMAGE_TYPE)]
     chunk_type: u32,
+    #[bw(assert(nominal_size == width.max(height)))]
     nominal_size: u32,
     #[bw(calc = IMAGE_VERSION)]
     version: u32,
 
-    // these checks are parroted from libxcursor
+    // original libxcursor checks for (w, h <= 32,767) but constrained it further
     // https://gitlab.freedesktop.org/xorg/lib/libxcursor/-/blob/master/include/X11/Xcursor/Xcursor.h.in?ref_type=heads#L183
-    // though they probably should be stricter...
-    #[bw(assert(*width != 0, *width <= 32_767))]
+    #[bw(assert(*width != 0, *width <= 2048))]
     width: u32,
-    #[bw(assert(*height != 0, *height <= 32_767))]
+    #[bw(assert(*height != 0, *height <= 2048))]
     height: u32,
     #[bw(assert(hotspot_x <= width))]
     hotspot_x: u32,
     #[bw(assert(hotspot_y <= height))]
     hotspot_y: u32,
 
-    /// Delay between frames. Set to zero if static.
+    /// The time this frame is displayed for before the next.
     delay_ms: u32,
 
     /// Pre-multiplied big-endian ARGB image data.
+    // despite this being big-endian, we can write
+    // in little endian with bgra quads equivalently
+    // so, NOTE: don't add #[bw(big)] to this
+    #[bw(assert(argb.len() == usize::try_from(width * height).unwrap()))]
     argb: Vec<u32>,
 }
 
-impl From<CursorImage> for ImageChunk {
-    fn from(image: CursorImage) -> Self {
+impl From<&CursorImage> for ImageChunk {
+    fn from(image: &CursorImage) -> Self {
         let mut rgba = image.rgba().to_owned();
         to_pre_argb(&mut rgba);
 
@@ -137,6 +148,7 @@ impl From<CursorImage> for ImageChunk {
 #[derive(Debug)]
 #[bw(little)]
 pub struct Xcursor {
+    #[bw(assert(header.toc.len() == images.len()))]
     header: XcursorHeader,
     images: Vec<ImageChunk>,
 }
@@ -144,7 +156,6 @@ pub struct Xcursor {
 /// Converts RGBA packed pixels to pre-multiplied big-endian ARGB in-place.
 ///
 /// If `rgba.len()` is not a multiple of four, the remainder is discarded.
-#[allow(clippy::cast_possible_truncation)]
 fn to_pre_argb(rgba: &mut [u8]) {
     for pixel in rgba.as_chunks_mut::<4>().0 {
         // write as LE-BGRA which is equivalent to BE-ARGB
@@ -183,17 +194,18 @@ impl Xcursor {
     ///
     /// ## Errors
     ///
-    /// If [`TryInto`] conversions fail. No other error can occur other than this.
+    /// If [`TryInto`] conversions fail between primitive
+    /// types. No other error can occur other than this.
     pub fn new(cursor: &GenericCursor) -> Result<Self> {
-        let num_toc = cursor.joined_images().count();
+        let num_toc = cursor.num_images();
         let num_toc_u32 = u32::try_from(num_toc)?;
         let toc_offset = XCURSOR_HEADER_SIZE;
         let image_offset = toc_offset + (num_toc_u32 * TOC_SIZE);
 
-        let mut toc: Vec<TableOfContents> = Vec::with_capacity(num_toc);
-        let mut images: Vec<ImageChunk> = Vec::with_capacity(num_toc);
-
+        let mut toc = Vec::with_capacity(num_toc);
+        let mut images = Vec::with_capacity(num_toc);
         let mut position = image_offset;
+
         for image in cursor.joined_images() {
             // make toc
             let toc_entry = TableOfContents {
@@ -203,13 +215,12 @@ impl Xcursor {
 
             toc.push(toc_entry);
 
-            // move corresponding image chunk position
-            // forward for next iteration
+            // move forward an image chunk for next toc
             position += IMAGE_HEADER_SIZE;
             position += u32::try_from(image.rgba().len())?;
 
             // make corresponding image chunk
-            images.push(image.to_owned().into());
+            images.push(image.into());
         }
 
         Ok(Self {
