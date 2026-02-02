fe37bc21e5c5e0c726b36cf33767b1566f47647a
hachispin <ganalonrainier255@gmail.com>
2026-02-02 17:57:52 +0000

    use `rayon` more efficiently (cpu more saturated)

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,51 @@ use currust::{
     cursors::{generic_cursor::GenericCursor, themes::CursorTheme},
 };
 
-use anyhow::{Result, bail};
+use anyhow::Result;
 use clap::Parser;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 fn main() -> Result<()> {
     let raw_args = Args::parse();
     let args = ParsedArgs::from_args(raw_args)?;
 
-    // NOTE: rayon not here yet...
-    for theme_dir in args.cursor_theme_dirs {
-        let mut theme = CursorTheme::from_theme_dir(&theme_dir)?;
+    args.cursor_theme_dirs.par_iter().try_for_each(|dir| {
+        let mut theme = CursorTheme::from_theme_dir(&dir)?;
 
-        args.scale_to.iter().try_for_each(|&sf| {
-            let algorithm = if sf > 1.0 {
+        for sf in &args.scale_to {
+            let algorithm = if *sf > 1.0 {
                 args.upscale_with
             } else {
                 args.downscale_with
             };
 
-            theme.add_scale(sf, algorithm, args.use_rayon)
-        })?;
+            theme.add_scale(*sf, algorithm)?;
+        }
 
         theme.save_as_x11_theme(&args.out)?;
-    }
 
-    for file in args.cursor_files {
-        let Some(ext) = file.extension() else {
-            bail!("no extension for file={}", file.display());
-        };
+        anyhow::Ok(())
+    })?;
 
-        let is_animated = ext == "ani";
-        let mut cursor = if is_animated {
-            GenericCursor::from_ani_path(&file)
-        } else {
-            GenericCursor::from_cur_path(&file)
+    for f in args.cursor_files {
+        let mut cursor = match f.extension() {
+            Some(v) if v == "cur" => GenericCursor::from_cur_path(f),
+            Some(v) if v == "ani" => GenericCursor::from_ani_path(f),
+            Some(_) | None => {
+                eprintln!("skipping {}", f.display());
+                continue;
+            }
         }?;
 
-        args.scale_to.iter().try_for_each(|&sf| {
-            let algorithm = if sf > 1.0 {
+        for sf in &args.scale_to {
+            let algorithm = if *sf > 1.0 {
                 args.upscale_with
             } else {
                 args.downscale_with
             };
 
-            cursor.add_scale(sf, algorithm)
-        })?;
-
-        let filename = file.file_stem().unwrap();
-        cursor.save_as_xcursor(&args.out.join(filename))?;
+            cursor.add_scale(*sf, algorithm)?;
+        }
     }
 
     Ok(())
