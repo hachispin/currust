fd78df45c295b2780cd021ad8b7ad80333751040
hachispin <ganalonrainier255@gmail.com>
2026-02-28 22:05:03 +0000

    add comment chunk writing support

--- src/formats/xcursor.rs
+++ src/formats/xcursor.rs
@@ -8,15 +8,13 @@
 //! 3) The `ImageChunk` s, which store **big-endian ARGB** pixel data,
 //!    along with other required metadata such as the hotspot.
 //!
-//! NOTE: Comment chunks aren't handled to make code
-//!       simpler, we're only _writing_, after all.
+//! Comment chunks may be written as well.
 //!
 //! ## References
 //!
 //! - [Xcursor](https://manpages.ubuntu.com/manpages/plucky/man3/Xcursor.3.html)
 //! - [libXcursor source](https://gitlab.freedesktop.org/xorg/lib/libxcursor)
 //! - [xcursorgen](https://gitlab.freedesktop.com/xorg/app/xcursorgen)
-// god i don't wanna write a parser again
 
 use crate::cursors::{cursor_image::CursorImage, generic_cursor::GenericCursor};
 
@@ -24,41 +22,53 @@ use anyhow::Result;
 use binrw::binwrite;
 use bytemuck;
 
-/* Sizes are needed for certain fields, which is also why they're u32.
- * An extra benefit is that they help in pointer calculations, since
- * binrw deletes calculated fields in structs, so size_of doesn't work. */
+/// Versions numbers. May be subject to change.
+mod versions {
+    pub const XCURSOR: u32 = 1 << 16;
+    pub const COMMENT: u32 = 1;
+    pub const IMAGE: u32 = 1;
+}
 
-/// Current Xcursor version. May need updating in the future.
-const XCURSOR_HEADER_VERSION: u32 = 1 << 16;
+/// Sizes (of fixed-size fields) for position calculations.
+mod sizes {
+    pub const XCURSOR: u32 = 16;
+    pub const COMMENT: u32 = 20;
+    pub const IMAGE: u32 = 36;
+    pub const TOC: u32 = 12;
+}
 
-/// Size of `XcursorHeader`, including elided fields and its magic, excluding `toc`.
-const XCURSOR_HEADER_SIZE: u32 = 16;
+#[binwrite]
+#[bw(repr = u32)]
+#[derive(Debug, Clone, Copy)]
+#[repr(u32)]
+enum ChunkType {
+    Comment = 0xfffe_0001,
+    Image = 0xfffd_0002,
+}
 
-/// Size of `TableOfContents`, including elided fields.
-const TOC_SIZE: u32 = 12;
-
-/// Size of `ImageChunk`, excluding `argb`.
-const IMAGE_HEADER_SIZE: u32 = 36;
-
-/// Magic value used to indicate a chunk is an `ImageChunk`.
-const IMAGE_TYPE: u32 = 0xfffd_0002;
-
-/// Current version stored in `ImageChunk`.
-const IMAGE_VERSION: u32 = 1;
+#[binwrite]
+#[bw(repr = u32)]
+#[derive(Debug, Clone, Copy)]
+#[repr(u32)]
+enum CommentRole {
+    Copyright = 1,
+    License = 2,
+    Other = 3,
+}
 
 /// Represents the file header for Xcursor files.
 #[binwrite]
-#[derive(Debug)]
 #[bw(little, magic = b"Xcur")]
+#[derive(Debug)]
 struct XcursorHeader {
-    #[bw(calc = XCURSOR_HEADER_SIZE)]
+    #[bw(calc = sizes::XCURSOR)]
     header_size: u32,
-    #[bw(calc = XCURSOR_HEADER_VERSION)]
+    #[bw(calc = versions::XCURSOR)]
     version: u32,
     #[bw(try_calc = toc.len().try_into())]
     num_toc: u32,
 
-    /// TOC entries which lead to [`ImageChunk`] s.
+    /// TOC entries which lead to comment/image chunks.
     toc: Vec<TableOfContents>,
 }
 
@@ -66,43 +76,67 @@ struct XcursorHeader {
 ///
 /// This should always lead to an [`ImageChunk`].
 #[binwrite]
-#[derive(Debug, Clone)]
 #[bw(little)]
+#[derive(Debug, Clone)]
 struct TableOfContents {
-    #[bw(calc = IMAGE_TYPE)]
-    r#type: u32,
-    /// The (image) chunk's nominal size, for matching.
-    ///
-    /// For example, for a 32x32 cursor, the nominal size
-    /// would most likely be 32. If dimensions aren't equal,
-    /// the largest dimension is chosen. (e.g, 48 for 48x32).
-    nominal_size: u32,
-    /// Offset where this entry's chunk is found.
-    ///
-    /// This should be where image data starts.
+    r#type: ChunkType,
+    /// Can be either the nominal size for
+    /// images or [`CommentRole`] for comments.
+    subtype: u32,
     position: u32,
+}
+
+#[binwrite]
+#[bw(little)]
+#[derive(Debug)]
+struct CommentChunk {
+    #[bw(calc = sizes::COMMENT)]
+    header_size: u32,
+    #[bw(calc = ChunkType::Comment)]
+    r#type: ChunkType,
+
+    role: CommentRole,
+
+    #[bw(calc = versions::COMMENT)]
+    version: u32,
+    #[bw(try_calc = string.len().try_into())]
+    length: u32,
+
+    /// The comment to be stored.
+    string: Vec<u8>,
+}
+
+impl CommentChunk {
+    fn new(string: String, subtype: CommentRole, position: u32) -> (Self, TableOfContents) {
+        let comment = Self {
+            role: subtype,
+            string: string.into_bytes(),
+        };
+
+        let toc = TableOfContents {
+            r#type: ChunkType::Comment,
+            subtype: comment.role as u32,
+            position,
+        };
+
+        (comment, toc)
+    }
 }
 
 /// Stores an image as [`Self::argb`], along with
 /// some additional metadata needed for cursors.
-///
-/// This also stores its own header, since there's no need
-/// to de-duplicate common fields with only one chunk type.
 #[binwrite]
-#[derive(Debug)]
 #[bw(little)]
+#[derive(Debug)]
 struct ImageChunk {
-    #[bw(calc = IMAGE_HEADER_SIZE)]
+    #[bw(calc = sizes::IMAGE)]
     header_size: u32,
-    #[bw(calc = IMAGE_TYPE)]
-    chunk_type: u32,
-    #[bw(calc = *width.max(height))]
+    #[bw(calc = ChunkType::Image)]
+    chunk_type: ChunkType,
     nominal_size: u32,
-    #[bw(calc = IMAGE_VERSION)]
+    #[bw(calc = versions::IMAGE)]
     version: u32,
 
-    // original libxcursor checks for (w, h <= 32,767) but constrained it further
-    // https://gitlab.freedesktop.org/xorg/lib/libxcursor/-/blob/master/include/X11/Xcursor/Xcursor.h.in?ref_type=heads#L183
     #[bw(assert(*width != 0, *width <= 2048))]
     width: u32,
     #[bw(assert(*height != 0, *height <= 2048))]
@@ -111,21 +145,34 @@ struct ImageChunk {
     hotspot_x: u32,
     #[bw(assert(hotspot_y <= height))]
     hotspot_y: u32,
-
-    /// The time (in milliseconds) that this
-    /// frame is displayed for before the next.
+    /// Uses milliseconds.
+    #[bw(assert(*delay <= 60_000))]
     delay: u32,
 
     /// Pre-multiplied big-endian ARGB image data.
-    // despite this being big-endian, we can write
-    // in little endian with bgra quads equivalently
-    // so, NOTE: don't add #[bw(big)] to this
+    // NOTE: Don't add #[bw(big)] to this.
     #[bw(assert(argb.len() == usize::try_from(width * height).unwrap()))]
     argb: Vec<u32>,
 }
 
+impl ImageChunk {
+    fn new(image: &CursorImage, position: u32) -> (Self, TableOfContents) {
+        let toc = TableOfContents {
+            r#type: ChunkType::Image,
+            // nominal size
+            subtype: image.nominal_size(),
+            position,
+        };
+
+        let image = Self::from(image);
+
+        (image, toc)
+    }
+}
+
 impl From<&CursorImage> for ImageChunk {
     fn from(image: &CursorImage) -> Self {
+        let nominal_size = image.nominal_size();
         let (width, height) = image.dimensions();
         let (hotspot_x, hotspot_y) = image.hotspot();
         let delay = image.delay();
@@ -135,6 +182,7 @@ impl From<&CursorImage> for ImageChunk {
         let argb = bytemuck::pod_collect_to_vec(&rgba);
 
         Self {
+            nominal_size,
             width,
             height,
             hotspot_x,
@@ -149,11 +197,12 @@ impl From<&CursorImage> for ImageChunk {
 ///
 /// This should produce a valid file when written.
 #[binwrite]
-#[derive(Debug)]
 #[bw(little)]
+#[derive(Debug)]
 pub struct Xcursor {
-    #[bw(assert(header.toc.len() == images.len()))]
+    #[bw(assert(header.toc.len() == images.len() + comment.as_ref().map_or(0, |_| 1)))]
     header: XcursorHeader,
+    comment: Option<CommentChunk>,
     images: Vec<ImageChunk>,
 }
 
@@ -189,33 +238,37 @@ impl Xcursor {
     /// If [`TryInto`] conversions fail between primitive
     /// types. No other error can occur other than this.
     pub fn new(cursor: &GenericCursor) -> Result<Self> {
-        let num_toc = cursor.num_images();
+        let num_toc = cursor.num_images() + cursor.info().map_or(0, |_| 1);
         let num_toc_u32 = u32::try_from(num_toc)?;
-        let toc_offset = XCURSOR_HEADER_SIZE;
-        let image_offset = toc_offset + (num_toc_u32 * TOC_SIZE);
+        let chunks_offset = sizes::XCURSOR + (num_toc_u32 * sizes::TOC);
 
         let mut toc = Vec::with_capacity(num_toc);
         let mut images = Vec::with_capacity(num_toc);
-        let mut position = image_offset;
+        let mut position = chunks_offset;
+
+        let comment = if let Some(info) = cursor.info() {
+            let info_len = u32::try_from(info.len())?;
+            let (chunk, toc_entry) = CommentChunk::new(info, CommentRole::Other, position);
+            position += sizes::COMMENT + info_len;
+            toc.push(toc_entry);
+            Some(chunk)
+        } else {
+            None
+        };
 
         for image in cursor.joined_images() {
-            let nominal_size = image.nominal_size();
-            let image_chunk_size = IMAGE_HEADER_SIZE + u32::try_from(image.rgba().len())?;
+            let image_chunk_size = sizes::IMAGE + u32::try_from(image.rgba().len())?;
+            let (chunk, toc_entry) = ImageChunk::new(image, position);
 
-            let toc_entry = TableOfContents {
-                nominal_size,
-                position,
-            };
-
-            debug_assert_eq!(toc_entry.nominal_size, image.nominal_size());
             toc.push(toc_entry);
-            images.push(ImageChunk::from(image));
+            images.push(chunk);
 
             position += image_chunk_size;
         }
 
         Ok(Self {
             header: XcursorHeader { toc },
+            comment,
             images,
         })
     }
@@ -259,7 +312,7 @@ mod test {
         }
 
         let frames = CursorImages::try_from(frames).unwrap();
-        let cursor = GenericCursor::new_unscaled(frames);
+        let cursor = GenericCursor::new_unscaled(frames, None);
         Xcursor::new(&cursor).unwrap()
     }
 
