130a12d9108c523b64f185786f36df41f114649b
hachispin <ganalonrainier255@gmail.com>
2026-08-07 17:50:40 +0100

    Resolve each component case-insensitively
    Initial implementation only resolved the filename, which worked
    under the original assumptions where cursors were stored in
    the same location as the INF file. This has now changed.

--- src/fs_utils.rs
+++ src/fs_utils.rs
@@ -1,60 +1,62 @@
 //! Utilities related to paths.
 
-use anyhow::{Result, anyhow, bail};
-use std::path::{Path, PathBuf};
+use anyhow::{Result, bail};
+
+use std::{
+    borrow::ToOwned,
+    path::{Path, PathBuf},
+};
 
 use crate::warn;
 
-/// Attempts to find `file_path` by searching through it's parent dir.
+/// Resolves each component in `path` case-insensitively.
 ///
-/// This does not search recursively. Also, `file_path.file_name()`
-/// not being found in its parent dir isn't considered an error.
+/// Mostly used for Windows to Linux path conversions.
 ///
 /// ## Errors
 ///
-/// - `file_path` doesn't have a parent
-/// - `file_path` is not a file
-/// - multiple files match `file_path`
-pub fn find_icase(file_path: &Path) -> Result<Option<PathBuf>> {
-    let file_path_display = file_path.display();
+/// If multiple candidates are found, or for general fs issues.
+pub fn resolve_icase(path: &Path) -> Result<Option<PathBuf>> {
+    let path_display = path.display();
 
-    if file_path.try_exists()? {
-        if file_path.metadata()?.is_file() {
-            return Ok(Some(file_path.to_path_buf()));
+    // windows should just always hit this, methinks
+    if path.try_exists()? {
+        return Ok(Some(path.to_path_buf()));
+    }
+
+    let mut resolved = PathBuf::new();
+
+    for component in path.components() {
+        if resolved.join(component).try_exists()? {
+            resolved.push(component);
+            continue;
         }
 
-        bail!("file_path={file_path_display} exists but is not a file");
+        let component = component.as_os_str();
+
+        let found: Vec<_> = read_dir(&resolved, true, true)?
+            .filter_map(|p| p.file_name().map(ToOwned::to_owned))
+            .filter(|name| name.eq_ignore_ascii_case(component))
+            .collect();
+
+        match found.as_slice() {
+            [] => return Ok(None),
+            [name] => resolved.push(name),
+            _ => bail!(
+                "multiple candidates found for case-insensitive \
+                lookup in parent={} for path={path_display}",
+                resolved.display()
+            ),
+        }
     }
 
-    let parent = file_path.parent().ok_or_else(|| {
-        anyhow!("no parent found for file_path={file_path_display} during case-insensitive lookup")
-    })?;
-    let parent_display = parent.display();
-
-    let filename = file_path
-        .file_name()
-        .ok_or_else(|| anyhow!("no filename for file_path={file_path_display}"))?;
-
-    let found: Vec<_> = read_dir_files(parent)?
-        .filter(|p| {
-            p.file_name()
-                .is_some_and(|name| name.eq_ignore_ascii_case(filename))
-        })
-        .collect();
-
-    if found.len() > 1 {
-        bail!(
-            "multiple candidates found for case-insensitive lookup \
-            in parent={parent_display} for filename={file_path_display}"
-        );
-    }
-
-    Ok(found.first().cloned())
+    Ok(Some(resolved))
 }
 
 /// Attempts to find files in `dir` with file extensions in `extensions`.
 ///
-/// This is case-insensitive and not recursive.
+/// This assumes that `dir` exists and does not search recursively for the given extensions.
+/// The extensions must _not_ be prefixed with a dot (e.g., "png" instead of ".png").
 ///
 /// ## Errors
 ///
@@ -69,16 +71,22 @@ pub fn find_extensions_icase(
         bail!("expected dir={dir_display} to be a directory");
     }
 
-    Ok(read_dir_files(dir)?.filter(|p| {
+    Ok(read_dir(dir, true, false)?.filter(|p| {
         p.extension()
             .is_some_and(|ext| extensions.iter().any(|ele| ext.eq_ignore_ascii_case(ele)))
     }))
 }
 
 /// Helper function for reading `dir` robustly.
-///
-/// The returned iterator only yields files.
-fn read_dir_files(dir: &Path) -> Result<impl Iterator<Item = PathBuf>> {
+fn read_dir(
+    dir: &Path,
+    allow_file: bool,
+    allow_dir: bool,
+) -> Result<impl Iterator<Item = PathBuf>> {
+    if !allow_file && !allow_dir {
+        bail!("both file and dir not allowed - most likely unintended");
+    }
+
     Ok(dir
         .read_dir()?
         .filter_map(|e| {
@@ -88,11 +96,11 @@ fn read_dir_files(dir: &Path) -> Result<impl Iterator<Item = PathBuf>> {
             .ok()
         })
         .map(|e| e.path())
-        .filter(|p| {
-            p.metadata()
+        .filter(move |p| {
+            p.metadata() // follows symlinks
                 .inspect_err(|err| {
                     warn!("failed to read metadata of path, p={}: {err}", p.display());
                 })
-                .is_ok_and(|m| m.is_file())
+                .is_ok_and(|m| allow_file && m.is_file() || allow_dir && m.is_dir())
         }))
 }
