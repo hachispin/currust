ec47353047b76272da074ce3a372997d03a9852a
hachispin <ganalonrainier255@gmail.com>
2026-08-09 20:00:20 +0100

    Remove unnecessary collect

--- src/formats/ani.rs
+++ src/formats/ani.rs
@@ -401,7 +401,7 @@ impl AniFile {
 
         if let Some(seq) = &ani.sequence
             && hdr.flags == Unsequenced
-            && seq.data != (0..hdr.num_steps).collect::<Vec<_>>()
+            && seq.data.iter().copied().eq(0..hdr.num_steps)
         {
             warn!(
                 "expected 'seq ' chunk to be None from flags={:?}, found the non \
