d1a218b63e83d3f02b4b4eb9fc52f717cbecc0ab
hachispin <ganalonrainier255@gmail.com>
2026-08-09 18:49:23 +0100

    Improve docs

--- src/formats/ani.rs
+++ src/formats/ani.rs
@@ -1,7 +1,7 @@
 //! Module for parsing [ANI](https://en.wikipedia.org/wiki/ANI_(file_format)).
 //!
-//! Note: "CUR" and "ICO" are used interchangeably, the
-//! only notable difference is the presence of a hotspot.
+//! Note that "CUR" and "ICO" are used interchangeably, since
+//! the only notable difference is the presence of a hotspot.
 //!
 //! You may find it helpful to also read about [RIFF](https://en.wikipedia.org/wiki/Resource_Interchange_File_Format).
 
@@ -15,7 +15,7 @@ use binrw::{BinRead, NullString, binread};
 
 use crate::warn;
 
-/// RIFF chunk with [`Self::data`] as `Vec<u32>`.
+/// RIFF chunk with [`Self::data`] as [`Vec<u32>`].
 #[binread]
 #[derive(Debug)]
 #[br(little)]
@@ -27,7 +27,6 @@ pub struct RiffChunkU32 {
     #[br(try_calc = usize::try_from(data_size / 4), temp)]
     data_length: usize,
 
-    /// The chunk data.
     #[br(count = data_length)]
     pub data: Vec<u32>,
     // no padding needed, data is inherently even (u32)
@@ -42,14 +41,14 @@ pub struct RiffChunkU8 {
     #[br(temp)]
     data_size: u32,
 
-    /// The chunk data.
     #[br(count = data_size, pad_after = data_size % 2)]
     pub data: Vec<u8>,
     // padding byte skipped with `pad_after`
 }
 
-/// Contains possible flag combinations for [`AniHeader`].
-/// These are used to describe the state of the "seq " chunk.
+// NOTE: this is storing the valid combinations of bitflags and are not meant to be composable.
+
+/// Contains possible flag combinations for [`AniHeader`], which describe the "seq " chunk.
 ///
 /// The ANI format defines these flags:
 ///
@@ -58,43 +57,26 @@ pub struct RiffChunkU8 {
 /// #define AF_SEQUENCE 0x2     // Animation is sequenced.
 /// ```
 ///
-/// All frames must be in ICO format in order to store the required
-/// cursor metadata (e.g, hotspot), so these are invalid flags:
+/// All of the frames must be in ICO format in order to store the
+/// required cursor metadata (e.g., hotspot), so these are invalid flags:
 ///
-/// - `0`: no flags set
+/// - `0`: no flags are set
 /// - `2`: frames are not ICO
 #[derive(Debug, Default, PartialEq, BinRead)]
 #[br(little)]
 #[br(repr = u32)]
 enum AniFlags {
-    // NOTE: this is storing the valid combinations of
-    // bitflags and are not meant to be composable.
+    /// Contains ICO frames that play in the order they're defined (no "seq " chunk).
+    #[default]
+    Unsequenced = 1,
     /// Contains ICO frames with a custom "seq " chunk,
     /// which defines the order frames should be played.
     ///
     /// This is mainly for optimizing repeated frames.
-    #[default]
-    Unsequenced = 1,
-    /// Contains ICO frames that play in the
-    /// order they're defined (no "seq " chunk).
     Sequenced = 3,
 }
 
 /// Models an ANI file's header (or the "anih" chunk).
-///
-/// ```text
-/// typedef struct {
-///     DWORD cbSizeof;  // Should be sizeof(ANIHEADER) = 36 bytes.
-///     DWORD cFrames;   // Number of frames in the frame list.
-///     DWORD cSteps;    // Number of steps in the animation loop.
-///
-///     DWORD cx, cy;              // Not used.
-///     DWORD cBitCount, cPlanes;  // Not used.
-///
-///     DWORD jifRate;  // Default display rate, in jiffies (1/60s).
-///     DWORD fl;       // AF_ICON should be set. AF_SEQUENCE is optional.
-/// } ANIHEADER;             
-/// ```
 #[binread]
 #[derive(Debug, Default, PartialEq)]
 #[br(little)]
@@ -103,27 +85,22 @@ pub struct AniHeader {
     anih_size: u32,
     #[br(assert(anih_size == header_size && header_size == 36), temp)]
     header_size: u32,
-    /// Number of frames in "fram" LIST.
-    ///
-    /// This is different from [`Self::num_steps`]:
+    /// Number of frames in "fram" LIST. Not to be confused with [`Self::num_steps`]:
     ///
     /// ```text
     /// sequence = [0, 1, 2, 1] => num_steps  = 4
     /// frames   = [0, 1, 2]    => num_frames = 3
     /// ```
     pub num_frames: u32,
-    /// Number of steps in the animation loop.
-    ///
-    /// This is different from [`Self::num_frames`]:
+    /// Number of steps in the animation loop. Not to be confused with [`Self::num_frames`]:
     ///
     /// ```text
     /// sequence = [0, 1, 2, 1] => num_steps  = 4
     /// frames   = [0, 1, 2]    => num_frames = 3
     /// ```
 
-    // contains unused fields: cx, cy, cBitCount, cPlanes
-    // spec says these should be zero/reserved, but windows
-    // doesn't check--they render the cursor regardless
+    // This padding contains the unused fields: `cx`, `cy`, `cBitCount`, `cPlanes`. Spec says these
+    // should be zero/reserved, but Windows doesn't check--the cursor is rendered regardless.
     #[br(pad_after = 16)]
     pub num_steps: u32,
 
@@ -133,39 +110,7 @@ pub struct AniHeader {
     flags: AniFlags,
 }
 
-/// Models an ANI file.
-///
-/// ```text
-/// RIFF('ACON'  // ANI files have the "ACON" identifier.
-///
-///     [LIST('INFO'                   
-///         [INAM(<ZSTR>)]  // Title.
-///         [IART(<ZSTR>)]  // Author.
-///     )]                             
-///
-///     'anih'(<ANIHEADER>)  // ANI file header.
-///
-///     // Rate table (array of jiffies). If the AF_SEQUENCE flag is set
-///     // then the count is ANIHEADER.cSteps, otherwise ANIHEADER.cFrames.
-///     ['rate'(<DWORD...>)]  
-///
-///     // Sequence table (array of frame index values). Should be present
-///     // when the AF_SEQUENCE flag is set. Count is ANIHEADER.cSteps.
-///     ['seq '(<DWORD...>)]  
-///
-///     LIST('fram'              // List of frames data. Count is ANIHEADER.cFrames.
-///        'icon'(<icon_data_1>) // Frame 1
-///        'icon'(<icon_data_2>) // Frame 2
-///        ...                   // Frame 3..(ANIHEADER.cFrames)
-///     )
-/// )
-/// ```
-///
-/// NOTE: The order shown here does not reflect how actual
-///       ANI files can choose to order their fields.
-///
-/// - Brackets around a chunk indicate that it's optional.
-/// - \<ZSTR\> indicates a null-terminated string.
+/// Models a parsed ANI file.
 #[derive(Default)]
 pub struct AniFile {
     /// The header, i.e, the "anih" chunk.
@@ -178,21 +123,16 @@ pub struct AniFile {
     pub author: Option<NullString>,
     /// Per-frame timings. Usually [`None`].
     ///
-    /// ## Explanation
-    ///
     /// rate:   `[t_0, t_1, t_2, ...]`\
     /// frames: `[f_0, f_1, f_2, ...]`
     ///
-    /// Each frame, `f_n`, is displayed for `t_n`
-    /// jiffies until `f_{n+1}` (modulo length).
+    /// Each frame, `f_n`, is displayed for `t_n` jiffies until `f_{n+1}` (modulo length).
     ///
-    /// The rate is applied **after sequencing**, so `frames`
-    /// is better said as the "display order", see [`Self::sequence`].
+    /// The rate is applied **after sequencing**, so `frames` is
+    /// better said as the "display order", see [`Self::sequence`].
     pub rate: Option<RiffChunkU32>,
     /// Stores frame indices to indicate the order in which
     /// frames are played. Frames can also be repeated.
-    ///
-    /// ## Explanation
     ///
     /// frames:         `[f_0, f_1, f_2, f_3, ...]`\
     /// sequence:       `[2, 3, 0, 0, 1, ...]`\
@@ -200,10 +140,9 @@ pub struct AniFile {
     pub sequence: Option<RiffChunkU32>,
     /// ICO frames. Each frame should have a hotspot.
     ///
-    /// Each ICO frame can contain multiple images, usually
-    /// for supporting different sizes.
+    /// Each ICO frame can contain multiple images, usually for supporting different sizes.
     ///
-    /// _(although redundant, since Windows scales cursors already.)_
+    /// _Although redundant, since Windows scales cursors already._
     pub ico_frames: Vec<RiffChunkU8>,
 }
 
@@ -226,22 +165,18 @@ impl AniFile {
 
     /// Parses `ani_blob`.
     ///
-    /// This is pretty complicated to parse (matches on fourcc)
-    /// because of the "constraint" (or more like freedom?)
-    /// of chunks being able to appearing in an arbitrary order.
-    ///
-    /// > [gdgsoft](https://www.gdgsoft.com/anituner/help/aniformat.htm):
-    /// > "Any of the blocks ("ACON", "anih", "rate", or "seq ") can appear in any order."
-    ///
     /// ## Errors
     ///
-    /// Parsing is quite tricky. There's a load of errors that can happen:
+    /// Parsing is quite tricky. Some errors that can happen are:
     ///
-    /// - overflow on calculations
+    /// - {over,under}flow on calculations
     /// - duplicate chunks
     /// - missing required chunks (e.g, no [`AniHeader`])
-    /// - blob lengths being unreasonably large (safety)
     /// - more complex invariants not being met, see [`Self::check_invariants`]
+    ///
+    /// ## References
+    ///
+    /// - [gdgsoft](https://www.gdgsoft.com/anituner/help/aniformat.htm)
     pub fn from_blob(ani_blob: &[u8]) -> Result<Self> {
         if ani_blob.len() > Self::MAX_CHUNK_SIZE {
             bail!(
@@ -415,23 +350,14 @@ impl AniFile {
             _ => bail!("unexpected list_id={list_id:?}"),
         }
 
-        // do NOT pad based on list_data_size here !!!
-        // the last subchunk is padded already, so padding here
-        // would pad *twice*, causing some annoying off-by-ones
-        //
-        //if list_data_size % 2 != 0 {
-        //    cursor.seek_relative(1)?;
-        //}
-
         Ok(())
     }
 
     /// Helper function for checking invariants, since Clippy
     /// is complaining about my function body length :(
     ///
-    /// Some checks produce warnings, while other produce errors.
-    /// This is a deliberate choice, as Windows still renders
-    /// files that the spec technically considers invalid.
+    /// Some checks produce warnings, while other produce errors. This is a deliberate
+    /// choice, as Windows still renders files that the spec technically considers invalid.
     fn check_invariants(ani: &Self) -> Result<()> {
         use AniFlags::*;
 
